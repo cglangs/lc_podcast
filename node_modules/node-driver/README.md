5dc5c404c103ba33280595b2ba63e395b9f3c413
cglangs <cdanegallagher@gmail.com>
2020-06-16 12:07:08 -0300

    make server

---
+++ node_modules/node-driver/README.md
@@ -0,0 +1,4 @@
+node-driver
+===========
+
+coming soon
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { makeAugmentedSchema } = require("neo4j-graphql-js");
+const { ApolloServer }  = require("apollo-server");
+const neo4j = require('neo4j-driver')
+
+
+
+
+
+
+const driver = neo4j.driver(
+  'bolt://localhost:7687',
+  neo4j.auth.basic('neo4j','password')
+);
+
+const typeDefs = `
+type Word {
+  text: String!
+}
+type User {
+	name: String!
+}
+`
+
+const schema = makeAugmentedSchema({
+  typeDefs
+});
+
+const server = new ApolloServer({
+  schema: schema,
+  context: { driver }
+});
+
+server.listen(3003, '0.0.0.0').then(({ url }) => {
+  console.log(`GraphQL API ready at ${url}`);
+});
