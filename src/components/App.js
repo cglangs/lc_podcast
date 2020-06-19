bc9190d7946b2bf2111dd5aed093e5529eb5d851
cglangs <cdanegallagher@gmail.com>
2020-06-18 21:17:25 -0300

    add add word function in front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag';
+import { useMutation } from 'urql';
+import '../styles/App.css';
+
+const POST_MUTATION = gql`
+  mutation PostMutation($wordText: String!) {
+    CreateWord(text: $wordText) {
+      text
+    }
+  }
+
+`
 
 function App() {
+
+  const [wordText, setWordText] = React.useState('')
+  
+  const [state, executeMutation] = useMutation(POST_MUTATION)
+  
+  const submit = React.useCallback(() => {
+    executeMutation({wordText})
+  }, [executeMutation, wordText])
+
   return (
     <div className="App">
       <header className="App-header">
-        
+         <input
+          className="mb2"
+          value={wordText}
+          onChange={e => setWordText(e.target.value)}
+          type="text"
+          placeholder="Enter word here"
+        />
+      <button
+        disabled={state.fetching}
+        onClick={submit}
+      >
+        Submit
+      </button>
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache'
+
+const cache = cacheExchange({})
+
+const client = new Client({
+  url: 'http://0.0.0.0:3003',
+  exchanges: [dedupExchange, cache, fetchExchange],
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider value={client}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
