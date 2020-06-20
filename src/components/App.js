d97864547610b74812e9dac9a4910b1581345c13
cglangs <cdanegallagher@gmail.com>
2020-06-20 13:04:06 -0300

    add features to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,76 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'urql'
-//import { useMutation } from 'urql';
 import '../styles/App.css';
+
+
+const GET_WORDS = gql`
+  query GetWords{
+      wordlist{
+          text
+      }
+  }
+`
+
+function App() {
+
+  const [sentenceWords, setSentenceWords] = React.useState([])
+  const [selectedWord, setSelectedWord] = React.useState('')
+  const [wordToTeach, setWordToTeach] = React.useState('')
+
+  const [result] = useQuery({ query: GET_WORDS })
+  const {data} = result
+  const appendWord = (newWord) => setSentenceWords(words => [...words, newWord])
+  const popWord = () => setSentenceWords(words => words.slice(0,-1))
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          <p style={{fontSize: "50px;"}}>{"Word being learned: " + wordToTeach}</p>
+          <div>
+          <p style={{fontSize: "30px;"}}>{sentenceWords.length ?  sentenceWords.join(''): null}</p>
+           <button
+            disabled={sentenceWords.length === 0}
+            onClick={popWord}
+            >
+            Backspace
+            </button>
+          </div>
+          <select onChange={e => setSelectedWord(e.target.value)}>
+            <option selected>Select Word</option>
+            {data ? data.wordlist.map(word => <option value={word.text}>{word.text}</option>) : null}
+            </select>
+           <button
+            onClick={wordToTeach.length ? () => appendWord(selectedWord) : () => setWordToTeach(selectedWord)}
+            >
+            {wordToTeach.length ? "Add" : "Learn"}
+            </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import { useMutation } from 'urql';
+
+
 
 /*const ADD_WORD = gql`
   mutation AddWord($wordText: String!) {
@@ -11,13 +79,8 @@ import '../styles/App.css';
     }
   }
 `*/
-const GET_WORDS = gql`
-  query GetWords{
-      wordlist{
-          text
-      }
-  }
-`
+
+
 
 /*const WordList = () => {
 
@@ -64,38 +127,3 @@ const GET_WORDS = gql`
   /*const submit = React.useCallback(() => {
     executeMutation({wordText})
   }, [executeMutation, wordText])*/
-
-function App() {
-
-  const [sentenceWords, setSentenceWords] = React.useState([])
-  const [result] = useQuery({ query: GET_WORDS })
-  const {data} = result
-  const appendWord = (newWord) => setSentenceWords(words => [...words, newWord])
-  const popWord = () => setSentenceWords(words => words.slice(0,-1))
-
-
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-          <div>
-          <p style={{fontSize: "30px;"}}>{sentenceWords.length ?  sentenceWords.join(''): null}</p>
-           <button
-            disabled={sentenceWords.length === 0}
-            onClick={popWord}
-            >
-            Backspace
-            </button>
-
-          </div>
-          <select onChange={e => appendWord(e.target.value)}>
-            {data ? data.wordlist.map(word => <option value={word.text}>{word.text}</option>) : null}
-            </select>
-      </header>
-    </div>
-  );
-}
-
-export default App;
