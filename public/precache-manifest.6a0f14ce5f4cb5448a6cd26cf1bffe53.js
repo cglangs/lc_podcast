2b9975e613f31fb774c393f681dcf9919578e28a
cglangs <cdanegallagher@gmail.com>
2020-10-05 15:30:09 -0300

    ran npm build and moved files

---
+++ public/precache-manifest.6a0f14ce5f4cb5448a6cd26cf1bffe53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cadd9e1316522ddf240ab99333ac73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e451eef871478bef60",
+    "url": "/static/css/main.dc3fd141.chunk.css"
+  },
+  {
+    "revision": "082749e01f652a66d903",
+    "url": "/static/js/2.73f9ca4a.chunk.js"
+  },
+  {
+    "revision": "86403920ee512e2dde1f65ddee5a3879",
+    "url": "/static/js/2.73f9ca4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e451eef871478bef60",
+    "url": "/static/js/main.3c8e2b9f.chunk.js"
+  },
+  {
+    "revision": "015d2f23c02a62093d97",
+    "url": "/static/js/runtime-main.9ae724a8.js"
+  }
+]);
