38940fe6332ff6d63ebdf6666fd46b2f77015564
cglangs <cdanegallagher@gmail.com>
2020-10-05 13:58:33 -0300

    testing for heroku

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { getToken } from './constants'
 
-const port = process.env.PORT || 3003;
 
 const httpLink = createHttpLink({
-  uri: 'http://0.0.0.0:' + port
+  uri: '/graphql'
 })
 
 
